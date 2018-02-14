01662874e3ecc57828ca90066cab1fa9a41af021
Degrandis <G,degrandis@hotmail.com>
2018-02-13 20:19:01 -0800

    Added some commentation still needs a readme.txt

--- A01_MVC/VisitorLog/VisitorLog/Controllers/VisitorController.cs
+++ A01_MVC/VisitorLog/VisitorLog/Controllers/VisitorController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Augustus DeGrandis
+//Bit 285 Assignment 1
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +16,7 @@ namespace VisitorLog.Controllers
         [HttpPost]
         public ActionResult Login(Visitor visitor)
         {
+            //Updates and saves the database with user information IP adress and Time of form submission
             visitor.LoginTime = DateTime.Now;
             visitor.IpAdress = Request.UserHostAddress;
             db.Visitors.Add(visitor);
